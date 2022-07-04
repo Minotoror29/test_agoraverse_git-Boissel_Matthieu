262e2731019cbc3d25b05dd1a72e3279c8753d2b
Minotoror29 <matthieu.boissel@laposte.net>
2022-07-04 23:51:26 +0200

    object placement

--- Test Agoraverse/Assets/Scripts/ItemPreview.cs
+++ Test Agoraverse/Assets/Scripts/ItemPreview.cs
@@ -7,16 +7,48 @@ public class ItemPreview : MonoBehaviour
     private GameManager gm;
     private PlaceItem pi;
 
+    [SerializeField] private MeshRenderer mRenderer;
+
     public GameObject item;
+
+    private List<GameObject> collisions;
 
     private void Start()
     {
         gm = GameManager.Instance;
         pi = gm.GetComponent<PlaceItem>();
+
+        collisions = new List<GameObject>();
+
+        mRenderer.material = pi.greenMaterial;
+
+        transform.position = pi.itemPosition;
     }
 
     private void Update()
     {
         transform.position = pi.itemPosition;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        collisions.Add(other.gameObject);
+
+        if (collisions.Count >= 1)
+        {
+            mRenderer.material = pi.redMaterial;
+            pi.canPlace = false;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        collisions.Remove(other.gameObject);
+
+        if (collisions.Count == 0)
+        {
+            mRenderer.material = pi.greenMaterial;
+            pi.canPlace = true;
+        }
     }
 }
--- Test Agoraverse/Assets/Scripts/PlaceItem.cs
+++ Test Agoraverse/Assets/Scripts/PlaceItem.cs
@@ -9,8 +9,13 @@ public class PlaceItem : MonoBehaviour
     private Vector3 mousePos;
 
     [SerializeField] private LayerMask groundLM;
+    [SerializeField] private LayerMask itemLM;
 
-    public Vector3 itemPosition;
+    [HideInInspector] public Vector3 itemPosition;
+    public Material greenMaterial;
+    public Material redMaterial;
+    [HideInInspector] public bool canPlace = true;
+    [SerializeField] private float rotationSpeed;
 
     private void Start()
     {
@@ -24,15 +29,46 @@ public class PlaceItem : MonoBehaviour
         Debug.DrawRay(ray.origin, ray.direction * 1000, Color.red);
 
         RaycastHit hit;
+
         if (Physics.Raycast(ray, out hit, Mathf.Infinity, groundLM))
         {
             itemPosition = hit.point;
 
-            if (Input.GetMouseButtonDown(0))
+            if (gm.selectedItem != null)
             {
-                Instantiate(gm.selectedItem.GetComponent<ItemPreview>().item, gm.selectedItem.transform.position, gm.selectedItem.transform.rotation);
-                Destroy(gm.selectedItem);
+                gm.selectedItem.transform.Rotate(new Vector3(0, Input.GetAxis("Mouse ScrollWheel") * rotationSpeed, 0));
+
+                if (Input.GetMouseButtonDown(0))
+                {
+                    if (!canPlace)
+                    {
+                        return;
+                    }
+
+                    Instantiate(gm.selectedItem.GetComponent<ItemPreview>().item, gm.selectedItem.transform.position, gm.selectedItem.transform.rotation);
+                    Destroy(gm.selectedItem);
+                }
             }
+        }
+        
+        if (gm.selectedItem == null)
+        {
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity, itemLM))
+            {
+                if (Input.GetMouseButtonDown(0))
+                {
+                    gm.selectedItem = Instantiate(hit.transform.GetComponent<Item>().itemPreview, itemPosition, hit.transform.rotation);
+                    Destroy(hit.transform.gameObject);
+                } else if (Input.GetMouseButtonDown(1))
+                {
+
+                }
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.Delete))
+        {
+            Destroy(gm.selectedItem);
         }
     }
 }
